57c007a62fe7a468a96d8694b0cc136529dcdbae
romilson8 <zero.barros@gmail.com>
2017-12-11 22:35:51 -0300

    commite develop

--- controller.py
+++ controller.py
@@ -5,7 +5,6 @@ import hashlib
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'thisissecret'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost/testeApi'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://matyyaxexjsmlg:8107604602e55661da27d1cac9e0ab04651a87ac4e6846101ca391cb383199c3@ec2-107-20-176-7.compute-1.amazonaws.com:5432/de7q9p7jklnn70'
 app.debug = True
 db.init_app(app)
