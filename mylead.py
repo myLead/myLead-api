5494ebf01ddd1722254ef9479dc15259892e37e9
Jose Romilson <zero.barros@gmail.com>
2017-12-13 12:50:48 -0300

    Serando os arquivos em pacotes

--- mylead.py
+++ mylead.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://matyyaxexjsmlg:8107604602e55661da27d1cac9e0ab04651a87ac4e6846101ca391cb383199c3@ec2-107-20-176-7.compute-1.amazonaws.com:5432/de7q9p7jklnn70'
 db = SQLAlchemy(app)
 
-from usuariocontroller import *
+from controler.usuariocontroller import *
 from dbhelp import * 
 
 
