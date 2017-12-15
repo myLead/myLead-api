21b1d980a3996d2c22cfd6310780eb0b5b72ba20
romilson8 <zero.barros@gmail.com>
2017-12-15 01:08:58 -0300

    adicionada a linha CORS(app, supports_credentials=True) para tentar resolver problema no CORS

--- mylead.py
+++ mylead.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://matyyaxexjsmlg:8107604602e55661da27d1cac9e0ab04651a87ac4e6846101ca391cb383199c3@ec2-107-20-176-7.compute-1.amazonaws.com:5432/de7q9p7jklnn70'
 db = SQLAlchemy(app)
 
